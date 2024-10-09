a23deb12cbeb741b1460ab9a1cbd1a4db120c0e9
Alex Lewis <adlewis@andrew.cmu.edu>
2024-10-09 18:46:19 -0400

    attempt at connecting new frontend additions to backend controller

--- src/controllers/write/search.js
+++ src/controllers/write/search.js
@@ -4,10 +4,36 @@ const api = require('../../api');
 const helpers = require('../helpers');
 
 const Search = module.exports;
+const Topics = require('../topics');
 
 Search.categories = async (req, res) => {
 	helpers.formatApiResponse(200, res, await api.search.categories(req, req.query));
 };
+
+Search.topicSearchCategory = async (req, res) => {
+	const { cid } = req.params;
+	const { query } = req.query;
+
+	if (!cid || !query) {
+		return res.status(400).json({ error: 'Category ID and query are required.' });
+	}
+
+	try {
+		const topics = await searchTopicsInCategory(cid, query);
+		return helpers.formatApiResponse(200, res, topics);
+	} catch (error) {
+		console.error('Search error:', error);
+		return res.status(500).json({ error: 'An error occurred while searching topics.' });
+	}
+};
+
+async function searchTopicsInCategory(cid, query) {
+	const results = await Topics.find({
+		categoryId: cid,
+		title: { $regex: query, $options: 'i' },
+	});
+	return results;
+}
 
 Search.roomUsers = async (req, res) => {
 	const { query, uid } = req.query;
