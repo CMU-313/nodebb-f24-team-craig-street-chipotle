e3d90c381e32757586981cb992932f1761257ce4
Yang Pan <yangp2@andrew.cmu.edu>
2024-11-13 21:14:16 -0500

    fix style

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // var request = require('request');
 
 const translatorApi = module.exports;
@@ -6,8 +7,9 @@ const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
 	const TRANSLATOR_API = 'https://nodebb-csc-translator-service.azurewebsites.net/';
-	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-	const data = await response.json();
+    // const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+    const data = await response.json();
 	// return [data["is_english"], data["translated_content"]];
-    return [data.is_english, data.translated_content];
+	return [data.is_english, data.translated_content];
 };
